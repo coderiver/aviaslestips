2d3d7cca040ec4c65455c77e2ddbedfa1b7f798b
Georgiy <goshazvir@gmail.com>
2014-05-14 18:33:16 +0300

    load

--- js/common.js
+++ js/common.js
@@ -6,6 +6,42 @@ head.ready(function() {
 	  loop: true
 	});
 
+	//select
+	function select() {
+		var el = $('.js-select');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).toggleClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	$(document).click(function() {
+		$('.js-select').removeClass('is-open');
+	});
+
 	// global var
 	var windowheight = $(window).height();
 	var page1ContentHeight = $(".page1 .page1__in").height();
@@ -87,4 +123,6 @@ head.ready(function() {
 	 heightPage8();
 	 heightPage9();
 	});
+
+
 });
