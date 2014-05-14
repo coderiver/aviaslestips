b860bca76b0957e0e2645466fea1f6db7d583765
Georgiy <goshazvir@gmail.com>
2014-05-14 13:33:08 +0300

    page 1-3

--- js/common.js
+++ js/common.js
@@ -9,7 +9,14 @@ head.ready(function() {
 	// global var
 	var windowheight = $(window).height();
 	var page1ContentHeight = $(".page1 .page1__in").height();
-
+	var page2 = $(".js-content2").height();
+	var page3 = $(".js-content3").height();
+	var page4 = $(".js-content4").height();
+	var page5 = $(".js-content5").height();
+	var page6 = $(".js-content6").height();
+	var page7 = $(".js-content7").height();
+	var page8 = $(".js-content8").height();
+	var page9 = $(".js-content9").height();
 
 	// heightPage1
 	function heightPage1(){
@@ -19,7 +26,23 @@ head.ready(function() {
 	}
 	heightPage1();
 
+	// heightPage2
+	function heightPage2(){
+		var page2Margin = ((windowheight - page2 - 100) / 2);
+		$(".page2 .page__content").css('padding-top', page2Margin);
+	}
+	heightPage2();
+
+	// heightPage3
+	function heightPage3(){
+		var page2Margin = ((windowheight - page2 - 100) / 2);
+		$(".page3 .page__content").css('padding-top', page2Margin);
+	}
+	heightPage3();
+
 	$(window).resize(function() {
 	 heightPage1();
+	 heightPage2();
+	 heightPage3();
 	});
 });
