268861d26930765e0b2e105537947c1ae8e0e649
Georgiy <goshazvir@gmail.com>
2014-05-14 11:37:10 +0300

    init

--- js/common.js
+++ js/common.js
@@ -1,3 +1,25 @@
 head.ready(function() {
-	console.log($('body').html());
+	// console.log($('body').html());
+	$(".main").onepage_scroll({
+	  sectionContainer: "section",
+	  responsiveFallback: 600,
+	  loop: true
+	});
+
+	// global var
+	var windowheight = $(window).height();
+	var page1ContentHeight = $(".page1 .page1__in").height();
+
+
+	// heightPage1
+	function heightPage1(){
+		var page1Margin = ((windowheight - page1ContentHeight - 200) / 2);
+		$(".page1 .page1__in").css('margin-top', page1Margin);
+		// alert(page1ContentHeight);
+	}
+	heightPage1();
+
+	$(window).resize(function() {
+	 heightPage1();
+	});
 });
