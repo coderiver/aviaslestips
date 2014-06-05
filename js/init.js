b937b5ba015820694132d57f9ede5531e7b6a3f8
akella <akella.a@gmail.com>
2014-06-05 16:57:09 +0300

    space

--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js", "js/lib/cycle.js", "js/lib/jquery.tools.min.js", "js/common.js");
+head.load("js/lib/jquery.js", "js/lib/cycle.js","js/lib/swipe.js", "js/lib/jquery.tools.min.js", "js/common.js");
